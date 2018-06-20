1fea4237aa00163c1c9aa86d8c7cb02d39cc9e39
Nicholas Powers <njpowers7915@gmail.com>
2018-06-20 17:25:32 +0000

    Created New Player Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,7 @@ class ApplicationController < Sinatra::Base
     get '/' do
         erb :home
     end
+
     
+
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,4 +6,13 @@ class PlayersController < ApplicationController
       enable :sessions
       set :session_secret, "secret"
     end
+
+    get '/players/new' do
+        @team = Helpers.current_team(session)
+        if Helpers.is_logged_in?(session) == false
+          redirect '/login'
+        else
+          erb :'players/new'
+        end
+    end
 end
