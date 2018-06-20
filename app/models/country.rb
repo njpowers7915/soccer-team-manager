8d0cca2f790c04626e1b841927421f91b4774328
Nicholas Powers <njpowers7915@gmail.com>
2018-06-20 15:27:22 +0000

    Added slug methods to all models

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,12 @@ class Country < ActiveRecord::Base
     has_many :players
     has_many :country_teams
     has_many :teams, through: :country_teams
+
+    def slug
+        self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    end
+
+    def self.find_by_slug(slug)
+        self.all.detect {|i| i.slug == slug}
+    end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,12 @@
 class Player < ActiveRecord::Base
     belongs_to :team
     belongs_to :country
+
+    def slug
+        self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    end
+
+    def self.find_by_slug(slug)
+        self.all.detect {|i| i.slug == slug}
+    end
 end
